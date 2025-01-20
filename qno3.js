8a4999d6c7f58890747554aba4d876071e404468
sneha <snehachaulagain75@gmail.com>
2025-01-20 07:23:32 +0545

    qno 2 and 3

---
+++ qno3.js
@@ -0,0 +1,14 @@
+let myString = "Sneha Chaulagain"; 
+let myNumber = 42;           
+let myBoolean = false;          
+let myObject = { name: "sneha", age: 21 }; 
+let myUndefined;               
+let myNull = null;             
+
+
+console.log("Type of myString:", typeof myString);
+console.log("Type of myNumber:", typeof myNumber); 
+console.log("Type of myBoolean:", typeof myBoolean); 
+console.log("Type of myObject:", typeof myObject); 
+console.log("Type of myUndefined:", typeof myUndefined);
+console.log("Type of myNull:", typeof myNull); 
